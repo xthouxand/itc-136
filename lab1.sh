0f8aff87528a25eb61bd61d29af8abb3b729d43a
xthouxand <ernestalleniv@gmail.com>
2017-04-04 14:47:29 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,1 +1,12 @@
 #This is  a comment
+
+pwd     # stands for 'Print Working Directory' and tells me where I am. (WHERE)
+
+whoami  # Tells me which user i am logged in as (WHO)
+
+ls      # Tells me what files are in the directory (WHAT)
+
+cd      # Allows me to change into other directories (Navigation)
+
+date     # What time is it on the machine (WHEN)
+
